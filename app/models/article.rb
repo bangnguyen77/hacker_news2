985efe16ec3d63221c4869271c2b23243da36062
Bang Nguyen <bangnv@gmail.com>
2016-09-08 00:03:33 -0700

    create unit tests for article_spec and comment_spec, update Article model, and create routes

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ActiveRecord::Base
+  has_many :comments
   validates :title, :link, :user, :points, :presence => true
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,4 +5,5 @@ describe Article do
   it { should validate_presence_of :link}
   it { should validate_presence_of :user}
   it { should validate_presence_of :points}
+  it { should have_many :comments }
 end
